9182f6c86072695a6e8d5bb23a50de377f4e5ffb
Desmond Hamilton Jr <45751791+DesHam25@users.noreply.github.com>
2024-08-12 15:16:53 -0400

    Add files via upload

---
+++ assets/JS/app.js
@@ -0,0 +1,178 @@
+// Dropdown Menu========================================>
+
+let menuToggle = document.querySelector('.menuToggle');
+let navigation = document.querySelector('.navigation');
+
+menuToggle.onclick = function () {
+  navigation.classList.toggle('active');
+};
+
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll('.pad');
+    this.playBtn = document.querySelector('.play');
+    this.currentKick = '../sounds/kick-classic.wav';
+    this.currentSnare = '../sounds/snare-acoustic01.wav';
+    this.currentHihat = '../sounds/hihat-acoustic01.wav';
+    this.kickAudio = document.querySelector('.kick-sound');
+    this.snareAudio = document.querySelector('.snare-sound');
+    this.hihatAudio = document.querySelector('.hihat-sound');
+    this.index = 0;
+    this.bpm = 150;
+    this.isPlaying = null;
+    this.selects = document.querySelectorAll('select');
+    this.muteBtns = document.querySelectorAll('.mute');
+    this.tempoSlider = document.querySelector('.tempo-slider');
+    // this is going to track our actual track here
+  }
+  activePad() {
+    this.classList.toggle('active');
+  }
+  repeat() {
+    let step = this.index % 9;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    // Loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      const led = bar.querySelector('.led');
+      // check if pads arc active
+      if (bar.classList.contains('active')) {
+        if (led) led.classList.add('active'); // Activate LED
+        // check each sound
+        if (bar.classList.contains('kick-pad')) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains('snare-pad')) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains('hihat-pad')) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      } else {
+        if (led) led.classList.remove('active'); // Deactivate LED
+      }
+    });
+    this.index++;
+  }
+  strart() {
+    const interval = (60 / this.bpm) * 1000;
+    // Check if it's playing
+    // NULL
+    if (this.isPlaying) {
+      // claer the interval
+      clearInterval(this.isPlaying);
+      console.log(this.isPlaying);
+      this.isPlaying = null;
+    } else {
+      this.isPlaying = setInterval(() => {
+        this.repeat();
+      }, interval);
+    }
+  }
+  updateBtn() {
+    // NULL
+    if (!this.isPlaying) {
+      this.playBtn.innerText = 'Stop';
+      this.playBtn.classList.add('active');
+    } else {
+      this.playBtn.innerText = 'Play';
+      this.playBtn.classList.remove('active');
+    }
+  }
+  changeSound(e) {
+    const selectionName = e.target.name;
+    const selectionValue = e.target.value;
+    console.log(selectionName);
+    switch (selectionName) {
+      case 'kick-select':
+        this.kickAudio.src = selectionValue;
+        break;
+      case 'snare-select':
+        this.snareAudio.src = selectionValue;
+        break;
+      case 'hihat-select':
+        this.hihatAudio.src = selectionValue;
+        break;
+    }
+  }
+  mute(e) {
+    const muteIndex = e.target.getAttribute('data-track');
+    e.target.classList.toggle('active');
+    if (e.target.classList.contains('active')) {
+      switch (muteIndex) {
+        case '0':
+          this.kickAudio.volume = 0;
+          break;
+        case '1':
+          this.snareAudio.volume = 0;
+          break;
+        case '2':
+          this.hihatAudio.volume = 0;
+          break;
+      }
+    } else {
+      switch (muteIndex) {
+        case '0':
+          this.kickAudio.volume = 1;
+          break;
+        case '1':
+          this.snareAudio.volume = 1;
+          break;
+        case '2':
+          this.hihatAudio.volume = 1;
+          break;
+      }
+    }
+  }
+  changeTempo(e) {
+    const tempoText = document.querySelector('.tempo-nr');
+    tempoText.innerHTML = e.target.value;
+    // console.log(e);
+  }
+  updateTempo(e) {
+    this.bpm = e.target.value;
+    clearInterval(this.isPlaying);
+    this.isPlaying = null;
+    const palyBtn = document.querySelector('.paly');
+    if (palyBtn.classList.contains('active')) {
+      this.strart();
+    }
+  }
+}
+
+const drumKit = new DrumKit();
+
+// Event Listeners
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener('click', drumKit.activePad);
+  pad.addEventListener('animationend', function () {
+    this.style.animation = '';
+  });
+});
+
+drumKit.playBtn.addEventListener('click', function () {
+  drumKit.updateBtn();
+  drumKit.strart();
+});
+
+drumKit.selects.forEach((select) => {
+  select.addEventListener('change', function (e) {
+    drumKit.changeSound(e);
+  });
+});
+
+drumKit.muteBtns.forEach((btn) => {
+  btn.addEventListener('click', function (e) {
+    drumKit.mute(e);
+  });
+});
+drumKit.tempoSlider.addEventListener('input', function (e) {
+  drumKit.changeTempo(e);
+});
+drumKit.tempoSlider.addEventListener('chane', function (e) {
+  drumKit.updateTempo(e);
+});
